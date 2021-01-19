1433f5912bb028274e6d4a3854d144b1b5ca3d1f
Marco Antonio <desarrollo.revan@libersalus.com>
2021-01-19 17:48:45 -0600

    Actualización de diseño.

--- index.php
+++ index.php
@@ -1,54 +1,108 @@
-<?php
-echo "Hola mundo";
-echo "Adios mundo";
-$title = 'PokeStats - Home';
+<!DOCTYPE html>
 
-include 'includes/views/head.php';
-include 'includes/functions/Request.php';
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Get Pokemon from ID</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/style.css">
 
-$pcNext = 20;
-$pcPrev = 0;
+  <script>
+		function validateID(){
+			var idEntered = document.forms["pokeForm"]["id"].value;
+			if (idEntered > 721 || idEntered < 1){
+				document.getElementById('errors').innerHTML="Enter number between 1 and 721.";
+				return false;
+			}
+			if (isNaN(idEntered)==true){
+				document.getElementById('errors').innerHTML="Must enter a number.";
+				return false;
+			}
+		}
+  </script>
 
-if (isset($_GET['offset'])) {
-    $offset = $_GET['offset'];
-    $pcNext = $offset + 20;
-    $pcPrev = $offset - 20;
-    $pokemons = json_decode(request("/pokemon?offset=$offset"));
-} else {
-    $pokemons = json_decode(request('/pokemon'));
-}
+</head>
+<body>	
 
-?>
-    <div class="wrapper">
-        <div class="container homelist">
-            <?php
-                if ($pokemons) {
-                    foreach ($pokemons->results as $pokemon):
-            ?>
-                <div class="item">
-                    <div class="content">
-                        <img src="<?= json_decode(request($pokemon->url, true))->sprites->front_default ?>" class="thumb"/>
-                        <div class="name"><?= $pokemon->name ?></div>
-                    </div>
-                    <a href="pokemon.php/?id=<?= json_decode(request($pokemon->url, true))->id ?>" class="btnmore">En savoir plus</a>
-                </div>
-            <?php endforeach; ?>
-        </div>
-            <div class="container">
-                <div class="page-control">
-                    <?php if (isset($_GET['offset'])) { ?>
-                        <a class="pc-btn" href="./?offset=<?= $pcPrev ?>">Previous</a>
-                    <?php } ?>
-                    <?php if ($pcNext < $pokemons->count) { ?>
-                        <a class="pc-btn" href="./?offset=<?= $pcNext ?>">Next</a>
-                    <?php } ?>
-                </div>
-            </div>
-        <?php
-            } else {
-                include_once 'includes/views/networkerror.php';
-            }
-        ?>
-    </div>
+  <div class="row">
+	<div class="col-md-6">
+		<div class="card">
+			<h2>Obtener pokemon por ID</h2>
+			<p>Ingresa el ID:</p>
+			<form name="pokeForm" action="" onsubmit="return validateID()" method="get">
+				<input type="text" name="id" placeholder="ID" required />
+				<div id="errors"></div>				 	
+				<input type="submit" class="btn btn-primary" value="Consultar"/>
+			</form>
+		</div>	
+	</div>
+
+	<div class="col-md-6">
+		<div class="card">
+			<h2>Obtener pokemon por nombre</h2>
+			<p>Ingresa el nombre:</p>
+			<form name="pokeForm" action="" onsubmit="return validateForm()" method="get">
+				<input type="text" name="name" placeholder="Nombre" required />
+				<div id="errors"></div>				 	
+				<input type="submit" class="btn btn-primary" value="Consultar"/>
+			</form>
+		</div>	
+	</div>
+  </div>
+ 
+  
+
+	<?php
+
+		if(ISSET($_GET['id'])){
+
+			$pokemonId = $_GET['id'];
+			$apiUrl = 'http://pokeapi.co/api/v2/pokemon/';
+			$pokemon = $apiUrl.$pokemonId;
+			$response = file_get_contents($pokemon);
+			$response = json_decode($response, true);
+			$sprite = $response['sprites']['front_default'];
+			echo "
+			<div class='row'>
+				<div class='col-md-6'>
+					<div class='card card-pokemon'>
+						<img src='$sprite'/>
+						<p>ID: {$response['id']} </p>  
+						<p>Nombre: {$response['name']} </p> 
+					</div>
+				</div>
+			</div>
+			<br>";	
+		}
+
+		if(ISSET($_GET['name'])){
+
+			$pokemonId = $_GET['name'];
+			$apiUrl = 'http://pokeapi.co/api/v2/pokemon/';
+			$pokemon = $apiUrl.$pokemonId;
+			$response = file_get_contents($pokemon);
+			$response = json_decode($response, true);
+			$sprite = $response['sprites']['front_default'];
+			echo "
+			<div class='row'>
+				<div class='col-md-6'>
+					<div class='card card-pokemon'>
+						<img src='$sprite'/>
+						<p>ID: {$response['id']} </p>  
+						<p>Nombre: {$response['name']} </p> 
+					</div>
+				</div>
+			</div>
+			<br>";
+		}
+
+	?>
+
+
 </body>
 </html>
+
+
+
